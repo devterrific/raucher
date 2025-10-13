f2548886abe9c0649664cabb9e26079b4332c2bb
Alim4737674 <148438263+Alim4737674@users.noreply.github.com>
2025-10-13 12:41:13 +0200

    Add initial player script and test scene setup
    Created a new test scene with a Player GameObject and camera in Unity. Added Player_Main.cs script with basic movement variables and structure for future player movement logic.

---
+++ Raucher Simulator/Assets/Script/Player/Player_Main.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class Player_Main : MonoBehaviour
+{
+    [Header("Movement")]
+    [Tooltip("Hier kommt rein wie schnell der Player am ende sein wird.")]
+    [SerializeField] private int movementspeed = 1;
+    [SerializeField] private int sprintspeed = 5;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Console.WriteLine(movementspeed + " UND " + sprintspeed); // Diese Zeile ist dafuer da damit der Editor nicht nervt weil wir die Variablen nicht benutzen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hier soll bitte aufgeräumt sein ~ Das bedeuted nur Methoden
+        
+
+    }
+
+    private void Movement()
+    {
+        // Hier kommt das Movement hin
+
+    }
+}
